8a08cd188c448c09f1da765df05d1bdb2fbdf0fa
kaneda73 <kaneda73@example.local>
2025-07-02 17:00:25 +0200

    fix(js): Correct supabase client initialization

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = 'https://aiguzywadjzyrwandgba.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZ3V6eXdhZGp6eXJ3YW5kZ2JhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyMDM1MzQsImV4cCI6MjA2Njc3OTUzNH0.pezVt3-xxkHBYK2V6ryHUtj_givF_TA9xwEzuK2essw';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: sessionStorage,
     autoRefreshToken: true,
